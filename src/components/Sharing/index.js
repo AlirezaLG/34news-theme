3ec8007b7ca4e0403ff22655baa7b2bae029c1cb
Alireza Akbari <a.r.akbari69@gmail.com>
2024-07-25 15:56:57 +0200

    fix

--- src/components/Sharing/index.js
+++ src/components/Sharing/index.js
@@ -24,10 +24,10 @@ export default function Sharing({ post, category = null, locale }) {
   let turl = "";
   if (category) {
     /* prettier-ignore */
-    const u = encodeURIComponent (decodeURIComponent(category)) +"/"+ encodeURIComponent(post?.slug);
-    console.log(u);
+    const u = category +"/"+ encodeURIComponent(post?.slug);
     turl = process.env.NEXT_PUBLIC_APP_URL + locale + "/posts/" + u;
   }
+  console.log("turl: " + turl);
 
   const { t } = useTranslation();
 
@@ -40,12 +40,7 @@ export default function Sharing({ post, category = null, locale }) {
     openInNewTab(shareUrl);
   };
 
-  // https://www.facebook.com/share_channel/?link=https%3A%2F%2F34news.com%2Fposts%2F%D8%AE%D8%A8%D8%B1-%D8%A7%D8%B5%D9%84%DB%8C%2F%D8%A7%D9%81%D8%B2%D8%A7%DB%8C%D8%B4-%DA%AF%D8%B1%D9%85%D8%A7-%D8%AF%D8%B1%D9%87%D9%86%D8%AF%D8%9B-%D8%A8%DB%8C%D8%B4-%D8%A7%D8%B2-%D8%AF%D9%88%D8%B5%D8%AF-%D8%AA%D9%86-%D8%AC%D8%A7%D9%86&app_id=966242223397117&source_surface=external_reshare&display&hashtag
-  // https://www.facebook.com/share_channel/?link=https%3A%2F%2F34news.com%2Fposts%2F%25D8%25AE%25D8%25A8%25D8%25B1-%25D8%25A7%25D8%25B5%25D9%2584%25DB%258C%2F%D8%A7%D9%81%D8%B2%D8%A7%DB%8C%D8%B4-%DA%AF%D8%B1%D9%85%D8%A7-%D8%AF%D8%B1%D9%87%D9%86%D8%AF%D8%9B-%D8%A8%DB%8C%D8%B4-%D8%A7%D8%B2-%D8%AF%D9%88%D8%B5%D8%AF-%D8%AA%D9%86-%D8%AC%D8%A7%D9%86&app_id=966242223397117&source_surface=external_reshare&display&hashtag
-
   const handleTwitterClick = () => {
-    /* prettier-ignore */
-
     const shareUrl = `https://twitter.com/intent/tweet?url=${turl}&text=${decodeURIComponent(
       post?.title
     )}`;
